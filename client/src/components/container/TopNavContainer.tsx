c9a6ef4e9de0465e9771058ac8a9220fce8d8e22
lalalalyt <lynneli318@gmail.com>
2022-06-01 20:27:01 -0400

    add authentication page

--- client/src/components/container/TopNavContainer.tsx
+++ client/src/components/container/TopNavContainer.tsx
@@ -1,12 +1,15 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import Authentication from "../topNav/Authentication";
+import {ModeContext} from "../../App";
+import { useContext } from "react";
 
 function TopNav() {
+  const [mode, transite] = useContext(ModeContext)
   
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h3" sx={{ flexGrow: 1 }}>SmartKitchen</Typography>
+        <Typography variant="h3" sx={{ flexGrow: 1 }} onClick={()=>transite("HOME")}>SmartKitchen</Typography>
         <Authentication />
       </Toolbar>
     </AppBar>
--- client/src/components/topNav/Authentication.tsx
+++ client/src/components/topNav/Authentication.tsx
@@ -1,7 +1,59 @@
-import { Button } from "@mui/material";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-function Authentication(){
-    return <Button color="inherit"> Login</Button>
+function Authentication() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button color="inherit" onClick={handleClickOpen}>
+        Login
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle >User Log In</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Getting Started to manage your kitchen smartly!
+          </DialogContentText>
+          <Button sx={{ border: 1, borderRadius: 2, mt: 2}}>Create new account</Button>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Log In</Button>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default Authentication;
